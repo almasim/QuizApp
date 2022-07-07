2eda539b04f493dbd1a340d581a4044b09cb8985
almasim <myl4n1@gmail.com>
2022-07-07 20:03:46 +0200

    i just do this to forget about my life so i can not be said while do this but every time i stop i get realy sad and feel like i should die because noone loves me nor likes me im just a waste of human flesh  i should be the one dying not the every other people i deserve nothing on this world please someone help me

---
+++ script.js
@@ -0,0 +1,40 @@
+const quizData=[
+{
+    question:'How old is the sun?',
+    a:'10',
+    b:'11',
+    c:'15',
+    d:'4,603E9',
+    correct:'d'
+}, {
+    question:'What is the most used programming language in 2019?',
+    a:"Java",
+    b:'C',
+    c:'Python',
+    d:'JavaScript',
+    correct:'a'
+}, {
+    question:'When is Christmass?',
+    a:"Dec 24",
+    b:'Dec 25',
+    c:'July 4',
+    d:'Aug 6',
+    correct:'b'
+}, {
+    question:'What does HTML stand for?',
+    a:"Hypertext Markup Language",
+    b:'Helicopter Terminals Motorboats Lamborginis',
+    c:'Jason Object Notation',
+    d:'Hyper Text Made up Language',
+    correct:'a'
+}, {
+    question:'What year was JavaScript launched',
+    a:"1998",
+    b:'1996',
+    c:'1994',
+    d:'None of the above',
+    correct:'d'
+}
+
+
+]
